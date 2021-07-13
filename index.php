298eba31fcd1d5e213aca83330b8354cefb04531
Mitesh The Mouse <44154255+miteshget@users.noreply.github.com>
2021-07-13 15:04:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <?php
   $db=mysqli_connect("appdb.example.com","devops","redhat","userdb");
   if (mysqli_connect_errno()){ 
-    echo "<h1> Failed to connect <h1>";}
+    echo "<h1> Failed to connect </h1>";}
   else{
     echo "<h1> <img src=images/ansible_bg.png alt='Ansible Automation Platform' width=300 height=100>";
     echo "You are connected to backend application server" . " : " . gethostname();
-    echo " </h1>"; }
+    echo " </h1><br />"; }
   $sql="SELECT username,userpass,usershell,userhome from login where username=\"$_GET[name]\"";
   if($result=mysqli_query($db,$sql)){
     while ($row=mysqli_fetch_row($result)){

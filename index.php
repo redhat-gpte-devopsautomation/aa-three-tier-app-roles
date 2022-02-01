bbc6a8d2fc110c1a8167c6a34adbf83d9ab2595f
Mitesh Sharma <mitsharm@redhat.com>
2022-02-01 14:06:21 +0530

    updated php

--- index.php
+++ index.php
@@ -8,19 +8,22 @@
       <?php
         $db=mysqli_connect("appdb.example.com","devops","redhat","userdb");
         if (mysqli_connect_errno()){ 
-          echo "<h1> Failed to connect </h1>";}
+          echo "<h1> Failed to connect </h1>";
+          exit();
+          }
         else{
           echo "<img src=images/ansible_bg.png alt='Ansible Automation Platform' width=30% height=30%>";
           echo "<br />";
           echo "<h1>You are connected to backend application server" . " : " . gethostname();
-          echo " </h1><br />"; }
+          echo " </h1><br />"; 
+          }
         $sql="SELECT username,userpass,usershell,userhome from login where username=\"$_GET[name]\"";
         if($result=mysqli_query($db,$sql)){
           while ($row=mysqli_fetch_row($result)){
             echo " <h2><br />";
             echo  $row[0] . " " . $row[1] ." " .  $row[2]. " ", $row[3]. "<br />";
-            exit();
-        }}
+            }
+          }
       ?>	
       <form action="<?php $_PHP_SELF?>" method="GET" >
         Enter User Name: <input type=text name="name" />
